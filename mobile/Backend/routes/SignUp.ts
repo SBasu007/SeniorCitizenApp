9332cd21959207696c8bcfaa86096f0390e9da45
Sourasish Biswas <sourasishbiswas7@gmail.com>
2025-07-14 20:24:56 +0530

    setup

--- mobile/Backend/routes/SignUp.ts
+++ mobile/Backend/routes/SignUp.ts
@@ -1,9 +1,27 @@
 import {Request,Response, Router} from "express";
+import { supabase } from "../supabase.server";
+
 
 const router = Router();
 
-router.get("/signup",async(req:Request,res:Response)=>{
-    return res.status(200).json({message:'router working'})
+router.post("/signup",async(req:Request,res:Response)=>{
+    const user_id = "something"
+    const user_email = "sometheing@gmail.com"
+    try{
+        const {data,error} = await supabase.from('users').insert([
+            {
+                user_id:user_id,
+                email:user_email
+            }
+        ])
+        if(error){
+            return res.status(500).json({message:"Error posting user signup details",error:error});
+        }
+
+        return res.status(200).json({message:"Data inserted succesfully",});
+    }catch(err){
+        return res.status(400).json({message:"Internal server error",error:err});
+    }
 })
 
 export default router;
